ed65e69655b14b357f65315ed81109c854c303cd
berkay22demirel <berkay_demirel22@hotmail.com>
2019-02-04 15:38:27 +0300

    first example

---
+++ beginRestEasy/src/firstexample/MessageController.java
@@ -0,0 +1,18 @@
+package firstexample;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/hello")
+public class MessageController {
+
+	@GET
+	@Path("/sayHello")
+	public String sayHello() {
+
+		String message = "Hello! Welcome to www.berkay22demirel.com";
+		return message;
+
+	}
+
+}
